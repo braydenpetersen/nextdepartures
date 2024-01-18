32f9b990a461d9573b4b0a41a48cdf26142dbff8
Brayden Petersen <115945971+braydenpetersen@users.noreply.github.com>
2024-01-18 18:31:06 -0500

    add file

---
+++ nearby_stops.py
@@ -0,0 +1,65 @@
+import requests
+import math
+from datetime import datetime
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+API_KEY = os.environ.get("API_KEY")
+LAT = os.environ.get("LAT")
+LON = os.environ.get("LON")
+currentTime = datetime.now().timestamp()
+localTime = datetime.now()
+
+print(f"\nDepartures for {localTime}")
+
+payload = {
+    'lat' : LAT,
+    'lon' : LON,
+    'max_distance' : 30
+}
+
+header = {"apiKey" : API_KEY}
+
+
+nearby_stops = requests.get(
+    "https://external.transitapp.com/v3/public/nearby_stops", params=payload, headers=header
+)
+
+
+for x in nearby_stops.json()['stops']:
+    stopId = [x][0]['global_stop_id']
+    stopName = [x][0]['stop_name']
+    stopCode = [x][0]['stop_code']
+
+    stopInfo = {
+        'global_stop_id' : stopId
+    }
+
+    departures = requests.get(
+        "https://external.transitapp.com/v3/public/stop_departures", params=stopInfo, headers=header
+    )
+
+    print("\n" + stopName + " [" + stopCode + "] " + stopId)
+    for i in range(3):
+        headsign = departures.json()['route_departures'][0]['itineraries'][0]['headsign'] # destination
+        route_short_name = departures.json()['route_departures'][0]['route_short_name'] # route name or number
+
+        # add extra detail if iON
+        if route_short_name == "301":
+            route_short_name = "301 iON"
+
+        currentDeparture = math.floor((departures.json()['route_departures'][0]['itineraries'][0]['schedule_items'][i]['departure_time']) - currentTime)
+        
+        timeUnit = "min"
+
+        if currentDeparture > 60:
+            currentDeparture = math.floor(currentDeparture/60)
+        else:
+            timeUnit ="s"
+
+        print(f"{route_short_name} to {headsign}\t\t{currentDeparture} {timeUnit}")
+
+    print("\n=========================================================")
