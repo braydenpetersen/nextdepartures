6bf45964d7b24276670ed56f1e3b0a71529a6934
Brayden Petersen <115945971+braydenpetersen@users.noreply.github.com>
2024-11-09 15:19:32 -0500

    testing frontend api call

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,40 +1,38 @@
 import React, { useEffect, useState } from "react";
 
 function index() {
-    const [departures, setDepartures] = useState({
-        time: "XX:XX",
-        routeNumber: "",
-        routeColor: "",
-        routeTextColor: "",
-        headsign: "",
-        global_stop_id: "",
-        stop_code: "",
-        branch_code: "",
-    }); // default departures structure
+    interface Departure {
+        departureTime: string;
+        routeNumber: string;
+        branch_code: string;
+        headsign: string;
+        platform: string;
+    }
+
+    const [departures, setDepartures] = useState<Departure[]>([]); // default departures structure
 
     useEffect(() => {
         fetch("http://localhost:8080/api/departures")
-            .then((res) => res.json())
-            .then((departures) => {
-                setDepartures({
-                    time: departures.time,
-                    routeNumber: departures.routeNumber,
-                    routeColor: departures.routeColor,
-                    routeTextColor: departures.routeTextColor,
-                    headsign: departures.headsign,
-                    global_stop_id: departures.global_stop_id,
-                    stop_code: departures.stop_code,
-                    branch_code: departures.branch_code,
-                });
+            .then((response) => response.json())
+            .then((data) => {
+                setDepartures(data);
+                console.log(data);
             });
     }, []);
 
     return (
         <div>
-            <h1>Departures</h1>
-            <p>{departures.time}</p>
+            <h1 >Departures</h1>
+            <ul>
+                {departures.map((departure, index) => (
+                    <li key={index}>
+                        {departure.departureTime} [{departure.routeNumber}{departure.branch_code}] to {departure.headsign} --- Platform {departure.platform}
+                    </li>
+                ))}
+            </ul>
         </div>
     );
+
 }
 
 export default index;
