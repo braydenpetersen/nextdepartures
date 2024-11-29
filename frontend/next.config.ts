129cff8e240e0b7409f20baa37fb60e192fb81c5
Brayden Petersen <115945971+braydenpetersen@users.noreply.github.com>
2024-11-28 21:51:16 -0500

    add vercel.json and setup for deployment

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: process.env.NODE_ENV === "development" 
+          ? "http://127.0.0.1:5000/api/:path*"  // Adjust the port if your Flask app uses a different one
+          : "/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
