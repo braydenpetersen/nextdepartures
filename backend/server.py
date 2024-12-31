25a54d5df4598cfc0b8df22b33534d01c78fda4c
Brayden Petersen <115945971+braydenpetersen@users.noreply.github.com>
2024-12-31 17:57:45 -0500

    more debugging

--- backend/server.py
+++ backend/server.py
@@ -19,7 +19,7 @@ STOP_CODE = "02799" # GO TRANSIT STOP CODE for University of Waterloo Station
 def get_GOtransit_departures():
     payload = {
         'StopCode': STOP_CODE,
-        'key': API_KEY
+        'key': os.environ.get('METROLINX_API_KEY')
     }
 
     print(f"Payload: {payload}")
